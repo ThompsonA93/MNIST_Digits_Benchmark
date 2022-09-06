313d72e2cfd9290b0892dc63d4ad13e9acb8ea84
Auer Thomas <Thomas.Auer@aau.at>
2022-09-06 14:10:56 +0200

    Restructuring - Too bloated ware; NN & PCA todo

--- python3/sklearn_neurnet/__init__.py
+++
@@ -1,6 +0,0 @@
-
-
-def run_sklearn_neurnet(x_train, y_train, x_test, y_test):
-    print("+-+-+-+-+ Executing SKLearn-Neural Net implementation +-+-+-+-+")
-
-    
--- python3/sklearn_svm/__init__.py
+++
@@ -1,7 +0,0 @@
-from sklearn import svm
-
-def run_sklearn_svm(x_train, y_train, x_test, y_test):
-    print("+-+-+-+-+ Executing SKLearn-SVM implementation +-+-+-+-+")
-
-    clf = svm.SVC()
-    clf.fit(x_train, y_train)
