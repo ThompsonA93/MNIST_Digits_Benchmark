8b1699729b0ed00847b22e3c601ea3b03d73c517
ThompsonA93 <thauer@edu.aau.at>
2022-09-08 13:57:44 +0200

    Quick fix on run.sh

--- RUN.sh
+++ RUN.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-cd src
-
-python3 01_MNIST_SVM.py
-python3 02_MNIST_SVM_PCA.py
-python3 03_MNIST_NN_SKLearn.py
-python3 04_MNIST_NN_Keras.py
+python3 src/01_MNIST_SVM.py
+python3 src/02_MNIST_SVM_PCA.py
+python3 src/03_MNIST_NN_SKLearn.py
+python3 src/04_MNIST_NN_Keras.py
